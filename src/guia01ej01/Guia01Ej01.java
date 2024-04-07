565bddcce3aa82063c7c20a08f46dcfc44609f59
Mona Lisa <asd@asd.com>
2024-04-07 17:54:00 -0500

    Primer Commit

---
+++ src/guia01ej01/Guia01Ej01.java
@@ -0,0 +1,19 @@
+/*
+Crear un proyecto de Java y definir al menos 6 variables en tu IDE de 
+distintos tipos de datos.
+ */
+package guia01ej01;
+
+public class Guia01Ej01 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+   int numero;
+   String nombre;
+   char letra ;
+   boolean falso;
+   double decimal;
+   float decimal2;
+    }
+    
+}
